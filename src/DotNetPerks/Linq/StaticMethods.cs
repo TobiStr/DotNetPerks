15f089dadc253afedda15cf91ac08af34437ee8b
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2021-07-29 18:34:45 +0200

    :1234: Add static Enumerable Methods

---
+++ src/DotNetPerks/Linq/StaticMethods.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+	public static class Extensions
+	{
+		/// <summary>
+		/// Returns an <see cref="IEnumerable{T}"/> from a list of parameters. Shorter than initializing a new array.
+		/// </summary>
+		public static IEnumerable<T> Of<T>(params T[] elements) =>
+			elements;
+
+		/// <summary>
+		/// Returns an <see cref="IDictionary{TKey, TValue}"/> from a list of parameters. Shorter than initializing a new dictionary.
+		/// </summary>
+		public static IDictionary<TKey, TValue> Map<TKey, TValue>(params (TKey, TValue)[] elements)
+		   where TKey : notnull
+		{
+			return elements.ToDictionary(
+				keySelector: e => e.Item1,
+				elementSelector: e => e.Item2
+			);
+		}
+	}
+}
