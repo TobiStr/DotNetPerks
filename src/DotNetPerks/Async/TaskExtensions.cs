a79c99ccb0415c2a82e434c09403d82e63a44292
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2021-07-29 18:33:50 +0200

    :star: Add Task Extensions

---
+++ src/DotNetPerks/Async/TaskExtensions.cs
@@ -0,0 +1,95 @@
+﻿namespace System.Threading.Tasks
+{
+	public static class TaskExtensions
+	{
+		/// <summary>
+		/// Maps a Task to another Task using the async selector function.
+		/// </summary>
+		/// <typeparam name="TIn"></typeparam>
+		/// <typeparam name="TOut"></typeparam>
+		/// <param name="task"></param>
+		/// <param name="selector"></param>
+		/// <returns></returns>
+		public static async Task<TOut> SelectAsync<TIn, TOut>(
+		   this Task<TIn> task,
+		   Func<TIn, Task<TOut>> selector
+		)
+		{
+			if (task is null)
+				throw new ArgumentNullException(nameof(task));
+
+			if (selector is null)
+				throw new ArgumentNullException(nameof(selector));
+
+			return await selector(await task);
+		}
+
+		/// <summary>
+		/// Maps a Task to another Task using the selector function.
+		/// </summary>
+		/// <typeparam name="TIn"></typeparam>
+		/// <typeparam name="TOut"></typeparam>
+		/// <param name="task"></param>
+		/// <param name="selector"></param>
+		/// <returns></returns>
+		public static async Task<TOut> SelectAsync<TIn, TOut>(
+			this Task<TIn> task,
+			Func<TIn, TOut> selector
+		)
+		{
+			if (task is null)
+				throw new ArgumentNullException(nameof(task));
+
+			if (selector is null)
+				throw new ArgumentNullException(nameof(selector));
+
+			return selector(await task);
+		}
+
+		/// <summary>
+		/// Executes an async function on the Task.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="task"></param>
+		/// <param name="action"></param>
+		/// <returns></returns>
+		public static async Task<T> DoAsync<T>(
+			this Task<T> task,
+			Func<T, Task> action
+		)
+		{
+			if (task is null)
+				throw new ArgumentNullException(nameof(task));
+
+			if (action is null)
+				throw new ArgumentNullException(nameof(action));
+
+			var res = await task;
+			await action(res);
+			return res;
+		}
+
+		/// <summary>
+		/// Executes an action on the Task.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="task"></param>
+		/// <param name="action"></param>
+		/// <returns></returns>
+		public static async Task<T> DoAsync<T>(
+			this Task<T> task,
+			Action<T> action
+		)
+		{
+			if (task is null)
+				throw new ArgumentNullException(nameof(task));
+
+			if (action is null)
+				throw new ArgumentNullException(nameof(action));
+
+			var res = await task;
+			action(res);
+			return res;
+		}
+	}
+}
---
+++ src/DotNetPerks/Async/TaskSequenceExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+	public static class TaskSequenceExtensions
+	{
+		/// <summary>
+		/// Executes and awaits every task of the sequence in order.
+		/// </summary>
+		public static async Task<IEnumerable<T>> WhenAllSequentialAsync<T>(this IEnumerable<Task<T>> tasks)
+		{
+			if (tasks is null)
+				throw new ArgumentNullException(nameof(tasks));
+
+			var results = new List<T>();
+			foreach (var task in tasks)
+				results.Add(await task.ConfigureAwait(false));
+			return results;
+		}
+
+		/// <summary>
+		/// Executes and awaits every task of the sequence in order.
+		/// </summary>
+		public static async Task WhenAllSequentialAsync(this IEnumerable<Task> tasks)
+		{
+			if (tasks is null)
+				throw new ArgumentNullException(nameof(tasks));
+
+			foreach (var task in tasks)
+				await task.ConfigureAwait(false);
+		}
+
+		/// <summary>
+		/// Executes and awaits every task of the sequence with no respect to the order.
+		/// </summary>
+		public static async Task<IEnumerable<T>> WhenAllAsync<T>(this IEnumerable<Task<T>> tasks)
+		{
+			if (tasks is null)
+				throw new ArgumentNullException(nameof(tasks));
+
+			return await Task
+				.WhenAll(tasks)
+				.ConfigureAwait(false);
+		}
+
+		/// <summary>
+		/// Executes and awaits every task of the sequence with no respect to the order.
+		/// </summary>
+		public static async Task WhenAllAsync(this IEnumerable<Task> tasks)
+		{
+			if (tasks is null)
+				throw new ArgumentNullException(nameof(tasks));
+
+			await Task
+				.WhenAll(tasks)
+				.ConfigureAwait(false);
+		}
+
+		/// <summary>
+		/// Applies an asynchronous selector on each element of the sequence.
+		/// </summary>
+		public static IEnumerable<Task<TOut>> Select<TIn, TOut>(
+			this IEnumerable<Task<TIn>> tasks,
+			Func<TIn, TOut> selector
+		)
+		{
+			return tasks.Select(t => t.SelectAsync(selector));
+		}
+	}
+}
