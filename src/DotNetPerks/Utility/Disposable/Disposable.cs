3621c6d0eeb22f97f74829ea09d75e03955e01b5
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2021-07-29 18:35:08 +0200

    :volcano: Add Disposable

---
+++ src/DotNetPerks/Utility/Disposable/Disposable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+	public class Disposable : IDisposable
+	{
+		private readonly List<Action> disposeActions =
+			new List<Action>();
+
+		public Disposable(Action disposeAction)
+		{
+			disposeActions.Add(
+				disposeAction ?? throw new ArgumentNullException(nameof(disposeAction))
+			);
+		}
+
+		public static IDisposable Create(Action dispose) =>
+			new Disposable(dispose);
+
+		public void Add(Action disposeAction) =>
+			disposeActions.Add(disposeAction);
+
+		public void Dispose()
+		{
+			foreach (var disposeAction in disposeActions)
+				disposeAction();
+		}
+	}
+}
