8eebd05fa3811bace38248bbaaffc8a6182866ca
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2021-07-29 18:35:29 +0200

    :abc: Add String Extensions

---
+++ src/DotNetPerks/Utility/String/StringEnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+	public static class StringEnumerableExtensions
+	{
+		public static string Join(this IEnumerable<string> strings, string separator)
+			=> string.Join(separator, strings);
+	}
+}
---
+++ src/DotNetPerks/Utility/String/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace System
+{
+	public static class StringExtensions
+	{
+		public static bool IsNullOrEmpty(this string input) =>
+			string.IsNullOrEmpty(input);
+
+		public static bool IsNullOrWhiteSpace(this string input) =>
+			string.IsNullOrWhiteSpace(input);
+
+		public static bool Equals(this string instance, string value) =>
+			string.Equals(instance, value, StringComparison.OrdinalIgnoreCase);
+	}
+}
