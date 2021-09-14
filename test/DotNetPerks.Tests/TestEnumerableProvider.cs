931c5f9a1db8c18a663bc34de8d53c1a35f5d34d
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2021-09-14 10:42:26 +0200

    :plus: Add TestEnumerableProvider

---
+++ test/DotNetPerks.Tests/TestEnumerableProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPerks.Tests.Utility
+{
+	internal static class TestEnumerableProvider
+	{
+		public static IEnumerable<int> GetTestEnumerable()
+		{
+			return Enumerable.Range(0, 100);
+		}
+
+		public static IEnumerable<Task<int>> GetTestAsyncEnumerable()
+		{
+			return Enumerable.Range(0, 100)
+				.Select(i => Task.FromResult(i));
+		}
+	}
+}
